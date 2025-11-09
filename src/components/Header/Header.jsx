08c71bdebf1279347aea83cfe5f541f49277fd82
C2theJ <coleblu84@gmail.com>
2025-11-09 10:28:37 -0500

    fixed ToggleSwitch component in the Header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 import CurrentUserContext from "../../contexts/CurrentUserContext";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 import "./Header.css";
 
 function Header({
@@ -9,38 +11,52 @@ function Header({
   handleLogout,
   isLoggedIn,
   weatherData,
+  currentTemperatureUnit,
+  handleToggleSwitchChange,
 }) {
   const currentUser = useContext(CurrentUserContext);
 
   return (
     <header className="header">
-      <div className="header__logo">WTWR</div>
+      <div className="header__container">
+        <Link to="/" className="header__logo">
+          WTWR
+        </Link>
 
-      {isLoggedIn && currentUser ? (
-        <nav className="header__nav">
-          <button onClick={handleAddClick}>Add Item</button>
-          <div className="header__user-info">
-            {currentUser.avatar ? (
-              <img
-                src={currentUser.avatar}
-                alt={currentUser.name}
-                className="header__avatar"
-              />
-            ) : (
-              <div className="header__avatar-placeholder">
-                {currentUser.name[0].toUpperCase()}
-              </div>
-            )}
-            <span className="header__username">{currentUser.name}</span>
-          </div>
-          <button onClick={handleLogout}>Logout</button>
-        </nav>
-      ) : (
-        <nav className="header__nav">
-          <button onClick={handleRegisterClick}>Sign Up</button>
-          <button onClick={handleLoginClick}>Sign In</button>
-        </nav>
-      )}
+        {isLoggedIn && currentUser ? (
+          <nav className="header__nav">
+            <Link to="/profile" className="header__link">
+              Profile
+            </Link>
+            <button onClick={handleAddClick}>Add Item</button>
+            <div className="header__user-info">
+              {currentUser.avatar ? (
+                <img
+                  src={currentUser.avatar}
+                  alt={currentUser.name}
+                  className="header__avatar"
+                />
+              ) : (
+                <div className="header__avatar-placeholder">
+                  {currentUser.name[0].toUpperCase()}
+                </div>
+              )}
+              <span className="header__username">{currentUser.name}</span>
+            </div>
+            <button onClick={handleLogout}>Logout</button>
+          </nav>
+        ) : (
+          <nav className="header__nav">
+            <button onClick={handleRegisterClick}>Sign Up</button>
+            <button onClick={handleLoginClick}>Sign In</button>
+          </nav>
+        )}
+
+        <ToggleSwitch
+          currentTemperatureUnit={currentTemperatureUnit}
+          handleToggleSwitchChange={handleToggleSwitchChange}
+        />
+      </div>
     </header>
   );
 }
